b7ba3240787a0c9d0b12073458480e717691a2f9
Maxime Thomassin <m.thomassin@rocketlab.co.nz>
2023-10-02 04:03:31 +0000

    added new color scheme material

--- config/neovim/starter/lua/plugins/colorscheme.lua
+++ config/neovim/starter/lua/plugins/colorscheme.lua
@@ -12,6 +12,9 @@ return {
   { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 
+  -- material
+  { "marko-cerovac/material.nvim", name = "material", lazy = false, priority = 1000 },
+
   -- catppucin
   {
     "catppuccin/nvim",
