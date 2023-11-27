f2947448ab8e314fd87e1f608856324afddcacbf
Maxime Thomassin <m.thomassin@rocketlab.co.nz>
2023-11-27 22:18:16 +0000

    more golang config

--- config/neovim/starter/lua/plugins/go.lua
+++ config/neovim/starter/lua/plugins/go.lua
@@ -84,16 +84,49 @@ return {
   },
 
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "goimports", "gofumpt" })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "delve" })
+    end,
+  },
+
+  {
     "nvimtools/none-ls.nvim",
     optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+        end,
+      },
+    },
     opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        local nls = require("null-ls")
-        vim.list_extend(opts.sources, {
-          nls.builtins.code_actions.gomodifytags,
-          nls.builtins.code_actions.impl,
-        })
-      end
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.code_actions.gomodifytags,
+        nls.builtins.code_actions.impl,
+        nls.builtins.formatting.goimports,
+        nls.builtins.formatting.gofumpt,
+      })
     end,
   },
 
@@ -117,7 +150,7 @@ return {
 
   {
     "leoluz/nvim-dap-go",
-     config = true,
+    config = true,
   },
 
   {
@@ -149,5 +182,4 @@ return {
       },
     },
   },
-
 }
